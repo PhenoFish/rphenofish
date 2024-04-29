1d3903e306ceab82689fc12706ce01c5a2683745
Nicolas Casajus <ahasverus@users.noreply.github.com>
2024-04-29 12:45:17 +0200

    clean: remove demo function

---
+++ tests/testthat/test-skeleton.R
@@ -0,0 +1,28 @@
+## Test skeleton ----
+
+### Data for tests ----
+
+# ...
+
+
+### Tests for errors ----
+
+test_that("Test funname() for error", {
+  
+  # expect_error(...,
+  #              "Error message",
+  #              fixed = TRUE)
+})
+
+
+### Tests for success ----
+
+test_that("Test funname() for success", {
+  
+  # expect_silent(x <- funname())
+  # 
+  # expect_true(is.data.frame(x))
+  # expect_equal(ncol(x), 4L)
+  # expect_equal(nrow(x), 1L)
+})
+
