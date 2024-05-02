dfc0a886d2af37c5b076fbda347499d46bd8f7cd
Nicolas Casajus <ahasverus@users.noreply.github.com>
2024-05-02 10:08:46 +0200

    minor: improve checks in correct_meow_polygons()

--- R/correct_meow_polygons.R
+++ R/correct_meow_polygons.R
@@ -4,45 +4,57 @@
 #' Some MEOW polygons cannot be projected without rendering artifact. This 
 #' function slightly changes longitude of polygons at the edge of World.
 #'
-#' @param meow a `sf MULTIPOLYGON` object. The MEOW spatial layer or the 
-#'   richness layer.
+#' @param data an `sf MULTIPOLYGON` object. The MEOW spatial layer.
 #'
 #' @return A `sf MULTIPOLYGON`.
 #' 
 #' @noRd
 
-correct_meow_polygons <- function(meow) {
+correct_meow_polygons <- function(data) {
+  
+  ## Check args ----
+  
+  error_if_not_sf(data)
+  error_if_field_not_in_df(data, "ECOREGION")
+  
+  
+  ## Correct polygons ----
   
   wrong_polygons <- c("Fiji Islands", "Gilbert/Ellis Islands")
   
-  pos <- which(meow$"ECOREGION" %in% wrong_polygons)
+  pos <- which(data$"ECOREGION" %in% wrong_polygons)
   
-  new_lines <- data.frame()
+  if (length(pos) > 0) {
+    
+    new_lines <- data.frame()
   
-  for (i in 1:length(pos)) {
-    
-    attrs  <- sf::st_drop_geometry(meow[pos[i], ])
-    coords <- sf::st_coordinates(meow[pos[i], ])
-    
-    n <- length(unique(coords[ , 4]))
-    
-    for (j in 1:n) {
+    for (i in 1:length(pos)) {
       
-      polygon <- coords[which(coords[ , 4] == j), ]
+      attrs  <- sf::st_drop_geometry(data[pos[i], ])
+      coords <- sf::st_coordinates(data[pos[i], ])
       
-      polygon[ , 1] <- as.numeric(gsub("180.0000", "179.9999", polygon[ , 1]))
+      n <- length(unique(coords[ , 4]))
       
-      polygon <- sf::st_polygon(list(polygon[ , 1:2]))
-      polygon <- sf::st_sfc(polygon, crs = sf::st_crs(4326))
-      new_line <- attrs
-      sf::st_geometry(new_line) <- polygon
-      
-      new_lines <- rbind(new_lines, new_line)
+      for (j in 1:n) {
+        
+        polygon <- coords[which(coords[ , 4] == j), ]
+        
+        polygon[ , 1] <- as.numeric(gsub("180.0000", "179.9999", polygon[ , 1]))
+        
+        polygon <- sf::st_polygon(list(polygon[ , 1:2]))
+        polygon <- sf::st_sfc(polygon, crs = sf::st_crs(4326))
+        new_line <- attrs
+        sf::st_geometry(new_line) <- polygon
+        
+        new_lines <- rbind(new_lines, new_line)
+      }
     }
+    
+    data <- data[-pos, ]
+    sf::st_geometry(new_lines) <- "geom"
+    
+    data <- rbind(data, new_lines)
   }
   
-  meow <- meow[-pos, ]
-  sf::st_geometry(new_lines) <- "geom"
-  
-  rbind(meow, new_lines)
+  data
 }
