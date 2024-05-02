44e91b04b4b7a3398df9c59e6e68be37b424a2a2
Nicolas Casajus <ahasverus@users.noreply.github.com>
2024-05-02 14:41:45 +0200

    feat: implement functions to check new types of args

--- R/checks.R
+++ R/checks.R
@@ -95,3 +95,84 @@ error_if_not_length_of <- function(x, n = 1) {
   
   invisible(NULL)  
 }
+
+
+
+#' Return an error if the argument is not a numeric
+#' 
+#' @noRd
+
+error_if_not_numeric <- function(x) {
+  
+  if (!is.numeric(x)) {
+    stop("Argument '", deparse(substitute(x)), "' must be a `numeric`", 
+         call. = FALSE)
+  }
+  
+  invisible(NULL) 
+}
+
+
+
+#' Return an error if the argument is not a logical
+#' 
+#' @noRd
+
+error_if_not_logical <- function(x) {
+  
+  if (!is.logical(x)) {
+    stop("Argument '", deparse(substitute(x)), "' must be a `logical`", 
+         call. = FALSE)
+  }
+  
+  invisible(NULL) 
+}
+
+
+
+#' Return an error if the argument is NULL
+#' 
+#' @noRd
+
+error_if_null <- function(x) {
+  
+  if (is.null(x)) {
+    stop("Argument '", deparse(substitute(x)), "' cannot be `NULL`", 
+         call. = FALSE)
+  }
+  
+  invisible(NULL) 
+}
+
+
+
+#' Return an error if the argument is/contains NA
+#' 
+#' @noRd
+
+error_if_na <- function(x) {
+  
+  if (any(is.na(x))) {
+    stop("Argument '", deparse(substitute(x)), "' cannot contain `NA`", 
+         call. = FALSE)
+  }
+  
+  invisible(NULL) 
+}
+
+
+
+#' Return an error if the argument is not strictly positive
+#' 
+#' @noRd
+
+error_if_not_strictly_positive <- function(x) {
+  
+  if (x <= 0) {
+    stop("Argument '", deparse(substitute(x)), "' must be strictly positive", 
+         call. = FALSE)
+  }
+  
+  invisible(NULL) 
+}
+
